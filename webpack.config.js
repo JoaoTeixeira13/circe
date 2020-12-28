cbf6a616efa5c0288ac6ae8f83bf4ee47447b3a8
David Friedman <david@spiced-academy.com>
2020-12-28 17:32:24 +0100

    set up

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = () => ({
+    entry: [
+        "@babel/polyfill",
+        path.join(__dirname, "client", "style.css"),
+        path.join(__dirname, "client", "src", "start.js"),
+    ],
+    output: {
+        path: path.join(__dirname, "client", "public"),
+        filename: "bundle.js",
+    },
+    performance: {
+        hints: false,
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "client", "public"),
+        proxy: {
+            "/": {
+                target: "http://localhost:3001",
+            },
+            "/socket.io": {
+                target: "http://localhost:3001",
+                ws: true,
+            },
+        },
+        port: "3000",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+            }, {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: 'bundle.css',
+    })],
+});
